4fdc203cb2dc28024823dc2f0884a4473839a270
rock <rick.anthony501@gmail.com>
2020-12-11 16:55:45 +0700

    MyNotesApp

---
+++ consumerapp/src/main/java/com/example/consumerapp/entity/Note.kt
@@ -0,0 +1,14 @@
+package com.example.consumerapp.entity
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+
+@Parcelize
+data class Note(
+        var id: Int = 0,
+        var title: String? = null,
+        var description: String? = null,
+        var date: String? = null
+
+) : Parcelable
